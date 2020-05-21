bbf315f7dd7d59366088ce4b4e444bd8ab799159
Nurçin <nrcinozer@gmail.com>
2020-05-22 00:41:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -35,8 +35,4 @@ app.get('/:shortUrl', async(req, res) => {
     res.redirect(shortUrl.full)
 });
 
-app.set( 'port', ( process.env.PORT || 5000 ));
-
-app.listen( app.get( 'port' ), function() {
-  console.log( 'Node server is running on port ' + app.get( 'port' ));
-});
+app.listen(process.env.PORT || 5000)
